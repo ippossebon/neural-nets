Added normalize and crossvalidation functions. Backpropagation needs fixes

main.py
@@ -4,6 +4,7 @@ import math
 import statistics
 import jsonpickle
 import pprint
+import matplotlib.pyplot as plt
 
 from neuralnetwork import NeuralNetwork
 
@@ -14,19 +15,53 @@ from neuralnetwork import NeuralNetwork
 print_multi_array = pprint.PrettyPrinter(indent=4)
 
 def main():
-    config_file = './data/configs/network.txt'
-    initial_weights_file = './data/configs/initial_weights.txt'
-    dataset_file = './data/datasets/wine.txt'
+    # config_file = './data/configs/network.txt'
+    # initial_weights_file = './data/configs/initial_weights.txt'
+    # dataset_file = './data/datasets/wine.txt'
+    #
+    # neurons_per_layer = [1, 2, 1]
+    # network = NeuralNetwork(
+    #     config_file=config_file,
+    #     dataset_file=dataset_file,
+    #     initial_weights_file=initial_weights_file,
+    #     neurons_per_layer=neurons_per_layer
+    # )
+    #
+    # network.backpropagation()
 
-    neurons_per_layer = [1, 2, 1]
-    network = NeuralNetwork(
-        config_file=config_file,
-        dataset_file=dataset_file,
-        initial_weights_file=initial_weights_file,
-        neurons_per_layer=neurons_per_layer
-    )
+    results_accuracy = []
+    results_precision = []
+    results_recall = []
+    results_f1measure = []
 
-    network.backpropagation()
+    k = 10
+    x = range(1,50)
+    x_axis = [1, 5, 10, 20, 25, 30, 35, 40, 45, 50]
+
+    for i in x_axis:
+        folds = getKStratifiedFolds(instances, target_class, k=k)
+        results = crossValidation(attributes,
+                                attributes_types,
+                                target_class,
+                                folds,
+                                b=1,
+                                k=k)
+
+        results_accuracy.append(results[0])
+        results_precision.append(results[1])
+        results_recall.append(results[2])
+        results_f1measure.append(results[3])
+
+    plt.xticks(np.arange(min(x), max(x)+1, 5.0))
+    plt.plot(x_axis, results_accuracy, label = "Accuracy")
+    plt.plot(x_axis, results_precision, label = "Precision")
+    plt.plot(x_axis, results_recall, label = "Recall")
+    plt.plot(x_axis, results_f1measure, label = "F1-Measure")
+    plt.ylabel('Values')
+    plt.xlabel('Number of trees')
+    plt.title('Results for' + file_name)
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     main()

neuralnetwork.py
@@ -4,7 +4,6 @@ import numpy as np
 from instance import Instance
 from utils import FileUtils
 
-
 class NeuralNetwork(object):
 
     def __init__(self, initial_weights_file, neurons_per_layer, dataset_file, config_file):
@@ -84,9 +83,29 @@ class NeuralNetwork(object):
         for g in range(len(self.neurons_per_layer) - 1):
             regularized_gradients.append(np.zeros(shape=(self.neurons_per_layer[g+1], self.neurons_per_layer[g]+1)))
 
-        ex1 = Instance(attributes=[0.13], classification=[0.9])
-        ex2 = Instance(attributes=[0.42], classification=[0.23])
-        training_data = [ex1, ex2]
+        #ex1 = Instance(attributes=[0.13], classification=[0.9])
+        #ex2 = Instance(attributes=[0.42], classification=[0.23])
+
+        #training_data = [ex1, ex2]
+
+        #training_data = self.dataset
+        training_data = []
+
+        # Normaliza o dataset
+        self.normalize()
+
+        for i in range(len(self.dataset)):
+            dataset_line = self.dataset[i].attributes
+
+            # calcula novo valor normalizado para cada parametro
+            for j in range(len(dataset_line)):
+                #print(len(dataset_line))
+                ex1 = Instance(attributes=dataset_line[j], classification=self.dataset[i].classification)
+                #print(self.dataset[i].classification)
+                training_data.append(ex1)
+
+        #print(training_data)
+
         delta = []
 
         y = []
@@ -110,7 +129,6 @@ class NeuralNetwork(object):
             # Propaga a instância e obtém as saídas preditas pela rede
             f_theta.append(training_data[i].classification)
             y.append(self.forwardPropagation(training_data[i]))
-
 
             # Calcula delta da camada de saída
             j_i = y[i] - f_theta[i]
@@ -194,8 +212,6 @@ class NeuralNetwork(object):
 
         # Retorna o custo regularizado J+S
 
-
-
     def forwardPropagation(self, instance):
         print('')
         # z é um vetor de tamanho num_layers
@@ -211,11 +227,10 @@ class NeuralNetwork(object):
         # Para cada camada k=2 (iniciando contagem em 1) até k=num_layers-1
         for k in range(1, self.num_layers-1):
             # Ativação dos neurônios da camada k
-            # z[k] = self.theta[k-1] * self.activation[k-1]
+            #z[k] = self.theta[k-1] * self.activation[k-1]
             z[k] = np.dot(self.theta[k-1], self.activation[k-1])
             a = g_vector_function(z[k])
             self.activation[k] = np.insert(a, 0, 1, axis=0)
-
 
         # Ativação do neurônio da camada de saída
         k = self.num_layers-1
@@ -232,10 +247,8 @@ class NeuralNetwork(object):
         # Predição final
         return self.activation[k]
 
-
     def g(self, x):
         return float(1/(1 + math.exp(-x)))
-
 
     def calculateError(self, f_theta, y):
         # calcula o erro obtido a partir das saídas obtidas para um exemplo i
@@ -248,3 +261,101 @@ class NeuralNetwork(object):
         J = first_term - second_term
 
         return J
+
+    # Normaliza as features dado o limite [0,1]
+    def normalize(self):
+        dataset = self.dataset
+        for i in range(len(dataset)):
+            dataset_line = dataset[i].attributes
+
+            min_value = np.min(dataset_line)
+            max_value = np.max(dataset_line)
+
+            # calcula novo valor normalizado para cada parametro
+            for j in range(len(dataset_line)):
+                dataset_line[j] = (dataset_line[j] - min_value)/(max_value - min_value)
+
+    # Cria o conjunto de bootstrap
+    def getBootstrap(data_set, size):
+        bootstrap = []
+
+        for i in range(size):
+            index = random.randint(0, len(data_set)-1)
+            bootstrap.append(data_set[index])
+
+        return bootstrap
+
+    # Executa a validacao cruzada estratificada
+    def crossValidation(attributes, attributes_types, target_class, folds, b, k):
+
+        config_file = './data/configs/network.txt'
+        initial_weights_file = './data/configs/initial_weights.txt'
+        dataset_file = './data/datasets/wine.txt'
+
+        accuracy_values = []
+        precision_values = []
+        recall_values = []
+        fmeasure_values = []
+
+        for i in range(k):
+            training_set_folds = list(folds)
+            training_set_folds.remove(folds[i])
+            training_set = transformToList(training_set_folds)
+
+            # bootstrap tem o tamanho do conjunto de treinamento
+            bootstrap_size = len(training_set)
+
+            test_set = folds[i]
+            forest = []
+
+            for j in range(b):
+                bootstrap = getBootstrap(training_set, bootstrap_size)
+                #tree = Tree(attributes, attributes_types, target_class, bootstrap)
+                #tree.createDecisionTree()
+
+                #forest.append(tree)
+
+                neurons_per_layer = [1, 2, 1]
+
+                network = NeuralNetwork(
+                    config_file=config_file,
+                    dataset_file=dataset_file,
+                    initial_weights_file=initial_weights_file,
+                    neurons_per_layer=neurons_per_layer
+                )
+
+                network.backpropagation()
+
+                forest.append(network)
+
+            # Usa o ensemble de B arvores para prever as instancias do fold i
+            # (fold de teste) e avaliar desempenho do algoritmo
+            true_positives, false_positives, false_negatives, true_negatives = evaluateForest(forest, test_set, target_class)
+
+            accuracy_values.append(calculateAccuracy(true_positives, true_negatives, false_positives, false_negatives))
+
+            precision_value = calculatePrecision(true_positives, false_positives)
+            precision_values.append(precision_value)
+
+            recall_value = calculateRecall(true_positives, false_negatives)
+            recall_values.append(recall_value)
+            fmeasure_values.append(calculateF1Measure(precision_value, recall_value))
+
+        accuracy = sum(accuracy_values)/len(accuracy_values)
+        precision = sum(precision_values)/len(precision_values)
+        recall = sum(recall_values)/len(recall_values)
+        fmeasure = sum(fmeasure_values)/len(fmeasure_values)
+
+        return accuracy, precision, recall, fmeasure
+
+    def calculateAccuracy(true_positives, true_negatives, false_positives, false_negatives):
+        return float((true_positives + true_negatives)/(true_positives + true_negatives + false_positives + false_negatives))
+
+    def calculateRecall(true_positives, false_negatives):
+        return float((true_positives)/(true_positives + false_negatives))
+
+    def calculatePrecision(true_positives, false_positives):
+        return float((true_positives)/(true_positives + false_positives))
+
+    def calculateF1Measure(precision, recall):
+        return float((2*precision*recall)/(precision+recall))