Add correct implementation for backpropagation

backpropagation/backpropagation.py
@@ -10,12 +10,7 @@ from neuralnetwork import NeuralNetwork
 """
 ./backpropagation network.txt initial_weights.txt dataset.txt
 
-config_file = '../data/configs/network2.txt'
-initial_weights_file = '../data/configs/initial_weights2.txt'
-dataset_file = '../data/datasets/wine.txt'
-
-
-python main.py ../data/configs/network.txt ../data/configs/initial_weights.txt ../data/datasets/wine.txt
+python backpropagation.py ../data/configs/network.txt ../data/configs/initial_weights.txt ../data/datasets/wine.txt
 """
 
 def main():
@@ -38,32 +33,16 @@ def main():
 
     reg_factor = config[0]
 
-    normalizeDataset(dataset)
-
     # Verifica a quantidade de classes distintas
     classes = []
     for instance in dataset:
         if instance.classification not in classes:
             classes.append(instance.classification)
 
-    # Altera o dataset para ter o numero de saidas como o numero de classes possiveis
-    for i in range(len(dataset)):
-        classification = dataset[i].classification
-        classification_dict = {}
-
-        for c in classes:
-            if c == classification:
-                # classificação da instancia
-                classification_dict[c] = 1
-            else:
-                classification_dict[c] = 0
-
-        dataset[i].classification = classification_dict
-
     # Inicializa rede com o número de atributos de cada instância como número de
     # neurônios na primeira camada e número de classes possíveis
     # como quantidade de neurônios da última camada
-    neurons_first_layer = len(dataset[i].attributes)
+    neurons_first_layer = len(dataset[0].attributes)
     neurons_last_layers = len(classes)
 
     neurons_per_layer = [neurons_first_layer, neurons_last_layers]
@@ -74,35 +53,32 @@ def main():
         neurons_per_layer.insert(index, layer)
         index+=1
 
+    regularization_factor = config[0]
+    print('Parametro de regularizacao lambda={0}\n'.format(regularization_factor))
+
+    neurons_per_layer_without_bias = list(neurons_per_layer)
+    for i in range(len(neurons_per_layer)):
+        if i == 0:
+            neurons_per_layer_without_bias[i] = neurons_per_layer[i]
+        else:
+            neurons_per_layer_without_bias[i] = neurons_per_layer[i] - 1
+
+    print('Inicializando rede com a seguinte estrutura de neuronios por camada: {0}\n'.format(neurons_per_layer_without_bias))
+
     network = NeuralNetwork(
         config_file = config_file,
         dataset = dataset,
         initial_weights_file = initial_weights_file,
-        neurons_per_layer = neurons_per_layer,
-        reg_factor = config[0]
+        neurons_per_layer = neurons_per_layer_without_bias,
+        reg_factor = regularization_factor
     )
 
-    network.backpropagation()
-    weights = network.runNetwork(max_iter=50)
+    print('\n\n--------------------------------------------')
+    print('Rodando backpropagation')
 
-    print('')
-    print('Pesos corretos = ')
-    print(weights)
+    network.runBackpropagation()
+    network.checkGradiends()
 
-    # print('\n \n \n')
-    # network.checkGradient()
-
-# Normaliza as features dado o limite [0,1]
-def normalizeDataset(dataset):
-    for i in range(len(dataset)):
-        dataset_line = dataset[i].attributes
-
-        min_value = np.min(dataset_line)
-        max_value = np.max(dataset_line)
-
-        # calcula novo valor normalizado para cada parametro
-        for j in range(len(dataset_line)):
-            dataset_line[j] = (dataset_line[j] - min_value)/(max_value - min_value)
 
 if __name__ == '__main__':
     main()

backpropagation/neuralnetwork.py
@@ -16,13 +16,13 @@ class NeuralNetwork(object):
         self.learning_rate = 0.005
         self.epsilon = 0.000001
 
-        self.training_data = None
+        self.training_data = dataset
 
         self.theta = []
-        self.initial_theta = []
         self.initTheta()
 
-        self.setTrainingData()
+        self.activation = []
+        self.initActivationsVector()
 
         self.regularized_gradients = []
 
@@ -32,8 +32,6 @@ class NeuralNetwork(object):
             # linhas = número de neuronios da proxima camada, colunas = número
             # de neuronios da camada atual + bias
             self.theta.append(np.zeros(shape=(self.neurons_per_layer[i+1], self.neurons_per_layer[i]+1)))
-            self.initial_theta.append(np.zeros(shape=(self.neurons_per_layer[i+1], self.neurons_per_layer[i]+1)))
-
 
         with open(self.initial_weights_file) as f:
             data = f.readlines()
@@ -50,9 +48,19 @@ class NeuralNetwork(object):
                     for neuronJ in range(len(i_weights)):
                         # neuronJ = neurônio neuronJ da camada atual
                         self.theta[layer_index][neuronI][neuronJ] = float(i_weights[neuronJ])
-                        self.initial_theta[layer_index][neuronI][neuronJ] = float(i_weights[neuronJ])
 
                 layer_index = layer_index + 1
+
+        for i in range(len(self.theta)):
+            print('\nTheta{0} inicial (pesos de cada neuronio, incluindo bias, armazenados nas linhas):'.format(i+1))
+            print(self.theta[i])
+
+
+        print('\n\nConjunto de treinamento')
+        for i in range(len(self.training_data)):
+            print('\tExemplo {0}'.format(i+1))
+            print('\t\tx: {0}'.format(self.training_data[i].attributes))
+            print('\t\ty: {0}'.format(self.training_data[i].classification))
 
 
     def initActivationsVector(self):
@@ -63,30 +71,17 @@ class NeuralNetwork(object):
             self.activation.append(np.zeros(shape=(self.neurons_per_layer[i]+1, 1)))
 
 
-    def setTrainingData(self):
-        # # Exemplo 1
-        ex1 = Instance(attributes=[0.13], classification=[0.9])
-        ex2 = Instance(attributes=[0.42], classification=[0.23])
-
-        # Exemplo 2
-        # ex1 = Instance(attributes=[0.32, 0.68], classification=[0.75, 0.98])
-        # ex2 = Instance(attributes=[0.83, 0.02], classification=[0.75, 0.28])
-
-        #self.training_data = [ex1, ex2]
-        self.training_data = self.dataset
-
-    def backpropagation(self, should_print=True):
-        should_print and print('--------------------------------------------')
-        should_print and print('Rodando backpropagation')
-
+    def backpropagation(self, thetas, should_print=True):
         delta = []
         y = []
         f_theta = []
         J_vector = []
         deltas = []
+        new_thetas = []
 
         for i in range(len(self.neurons_per_layer)-1):
-            deltas.append(np.zeros(shape=(self.theta[i].shape)))
+            deltas.append(np.zeros(shape=(thetas[i].shape)))
+            new_thetas.append(np.zeros(shape=(thetas[i].shape)))
 
         last_layer_index = len(self.neurons_per_layer) - 1
 
@@ -98,13 +93,20 @@ class NeuralNetwork(object):
             D_matrix.append(np.zeros(shape=(self.neurons_per_layer[g+1], self.neurons_per_layer[g]+1)))
 
         for i in range(len(self.training_data)):
+            should_print and print('\tProcessando exemplo de treinamento {0}'.format(i+1))
+
             # Propaga a instância e obtém as saídas preditas pela rede
             y.append(self.training_data[i].classification)
-            f, activation = self.forwardPropagation(self.training_data[i], should_print)
-            f_theta.append(f)
+            f_theta.append(self.forwardPropagation(self.training_data[i], thetas, should_print=should_print))
 
-            should_print and print('Saida esperada para o exemplo {0} = {1}'.format(i, y[i]))
-            should_print and print('Saida predita para o exemplo {0} = {1}'.format(i, f_theta[i]))
+            should_print and print('\tSaida predita para o exemplo {0} = {1}'.format(i, y[i]))
+            should_print and print('\tSaida esperada para o exemplo {0} = {1}'.format(i, f_theta[i]))
+
+            # Calcula o custo
+            j_i = self.cost(f_theta[i], y[i])
+            J_vector.append(j_i)
+
+            should_print and print('\tJ do exemplo {0} = {1}'.format(i+1, j_i))
 
             # Calcula delta da camada de saída
             j_i = f_theta[i] - y[i]
@@ -113,14 +115,16 @@ class NeuralNetwork(object):
             # Calcula delta das camadas ocultas
             for k in range(last_layer_index-1, 0, -1):
                 #  [θ(l=k)]T 𝛿(l=k+1) .* a(l=k) .* (1-a(l=k))
-                theta_copy = list(self.theta[k])
+                theta_copy = list(thetas[k])
+                # theta_matrix = np.delete(theta_copy, 0, axis=1) # remove o peso do neurônio de bias
                 theta_matrix = np.matrix(theta_copy)
 
                 theta_transp = theta_matrix.transpose()
-                delta_matrix = np.asmatrix(deltas[k+1]).transpose()
+                delta_matrix = np.asmatrix(deltas[k+1]).transpose() # TODO: TIVE QUE FAZER P conjunto 2
                 first_term = theta_transp * delta_matrix
 
-                activation_copy = np.array(activation[k])
+                activation_copy = np.array(self.activation[k])
+                # activation_matrix = np.delete(activation_copy, 0, axis=0) # remove a ativação do neurônio de bias
                 activation_matrix = np.matrix(activation_copy)
                 second_term = np.multiply(first_term, activation_matrix)
 
@@ -137,21 +141,16 @@ class NeuralNetwork(object):
             for k in range(last_layer_index-1, -1, -1):
                 # D(l=k) = D(l=k) + 𝛿(l=k+1) [a(l=k)]T
                 delta_matrix = np.matrix(deltas[k+1])
-                activation_matrix = np.matrix(activation[k])
+                activation_matrix = np.matrix(self.activation[k])
 
                 gradients = delta_matrix.transpose() * activation_matrix
                 D_matrix[k] = D_matrix[k] + gradients
 
-                should_print and print('Gradientes de theta {0} para o exemplo {1}'.format(k, i))
+                should_print and print('\nGradientes de theta {0} para o exemplo {1}'.format(k+1, i+1))
                 should_print and print(gradients)
 
-            # Calcula o custo
-            j_i = self.cost(f_theta[i], y[i])
-            J_vector.append(j_i)
 
-            should_print and print('J do exemplo {0} = {1}'.format(i, j_i))
-
-        should_print and print('\nDataset completo processado. Calculando gradientes regularizados...')
+        should_print and print('\n\nDataset completo processado. Calculando gradientes regularizados...')
         # Calcula gradientes finais (regularizados) para os pesos de cada camada
         # Inicializa vetor p
         p_vector = []
@@ -163,7 +162,7 @@ class NeuralNetwork(object):
 
         for k in range(last_layer_index-1, -1, -1):
             # Seja P(l=k) igual à (λ .* θ(l=k)), mas com a primeira coluna zerada -> aplica regularização λ apenas a pesos não bias
-            p_vector[k] = np.multiply(self.reg_factor, self.theta[k])
+            p_vector[k] = np.multiply(self.reg_factor, thetas[k])
 
             # Zera a primeira coluna -> não devemos multiplicar os pesos de bias
             p_vector[k][:,0] = 0
@@ -171,76 +170,74 @@ class NeuralNetwork(object):
             # combina gradientes com regularização; divide por #exemplos para calcular gradiente médio
             # D(l=k) = (1/n) (D(l=k) + P(l=k))
             self.regularized_gradients[k] = (1/len(self.training_data)) * (D_matrix[k] + p_vector[k])
-            should_print and print('Gradientes finais de theta (com regularização) {0} para o exemplo {1}'.format(k, i))
+            should_print and print('\nGradientes finais de theta{0} (com regularização)'.format(k+1))
             should_print and print(self.regularized_gradients[k])
 
 
         # Calcula custo total da rede
         J = float(sum(J_vector)/len(self.training_data))
 
+
         if self.reg_factor > 0:
             # Calcula S = eleva cada peso da rede ao quadrado (exceto os pesos de bias) e os soma
             s_factor = 0
             for k in range(last_layer_index-1, -1, -1):
-                s_factor = s_factor + np.sum(np.power(self.theta[k][:, 1:], 2))
+                s_factor = s_factor + np.sum(np.power(thetas[k][:, 1:], 2))
 
             s_factor = float((self.reg_factor/(2 * len(self.training_data)))) * s_factor
 
             # Calcula o custo regularizado
             J = J + s_factor
 
-        should_print and print('J total do dataset = {0}'.format(J))
+        should_print and print('J total do dataset (com regularização) = {0}\n'.format(J))
 
         # Atualiza pesos de cada camada com base nos gradientes
         for k in range(last_layer_index-1, -1, -1):
             # θ(l=k) = θ(l=k) - α .* D(l=k)
-            self.theta[k] = np.multiply(self.theta[k], self.learning_rate * D_matrix[k])
+            new_thetas[k] = np.multiply(thetas[k], self.learning_rate * D_matrix[k])
 
         return J
-        
-    def forwardPropagation(self, instance, should_print=True):
+
+    def forwardPropagation(self, instance, thetas, should_print=True):
         z = [0 for i in range(self.num_layers)]
         bias = [1]
 
-        should_print and print('\n--> Propagando entrada: {0}'.format(instance.attributes))
+        should_print and print('\tPropagando entrada: {0} \n'.format(instance.attributes))
 
-        activation = []
-        # activation[L][j] = ativação/saída do neurônio j da camada L
-        for i in range(len(self.neurons_per_layer)):
-            # activation é uma lista de arrays Nx1, onde N=número de neurônios da
-            # camada + 1 (bias)
-            activation.append(np.zeros(shape=(self.neurons_per_layer[i]+1, 1)))
-
-        activation[0] = bias + instance.attributes
+        self.activation[0] = bias + instance.attributes
 
         g_vector_function = np.vectorize(self.g)
 
         # Para cada camada k=2 (iniciando contagem em 1) até k=num_layers-1
         for k in range(1, self.num_layers-1):
             # Ativação dos neurônios da camada k
-            z[k] = np.dot(self.theta[k-1], activation[k-1])
-
+            #z[k] = self.theta[k-1] * self.activation[k-1]
+            z[k] = np.dot(thetas[k-1], self.activation[k-1])
             aux = g_vector_function(z[k])
-            activation[k] = np.insert(aux, 0, 1, axis=0)
+            self.activation[k] = np.insert(aux, 0, 1, axis=0)
 
         # Ativação do neurônio da camada de saída
         k = self.num_layers-1
 
-        z[k] = np.dot(self.theta[self.num_layers-2], activation[self.num_layers-2])
+        z[k] = np.dot(thetas[self.num_layers-2], self.activation[self.num_layers-2])
 
-        activation[k] = g_vector_function(z[k])
+        self.activation[k] = g_vector_function(z[k])
 
-        should_print and print('Vetor a =')
-        should_print and print(activation)
-        should_print and print('Vetor z =')
-        should_print and print(z)
-        should_print and print('Vetor de theta =')
-        should_print and print(self.theta)
-        should_print and print('Predicao final =')
-        should_print and print(activation[k])
+        should_print and print('\t\tVetor a =')
+        for i in range(len(self.activation)):
+            should_print and print('\t\t\t{0}'.format(list(self.activation[i])))
+
+        should_print and print('\t\tVetor z =')
+        for item in z:
+            try:
+                should_print and print('\t\t\t{0}'.format(list(item)))
+            except:
+                should_print and print('\t\t\t{0}'.format(item))
+
+        should_print and print('\t\tf(x) = {0} \n'.format(self.activation[k]))
 
         # Predição final
-        return activation[k], activation
+        return self.activation[k]
 
 
     def g(self, x):
@@ -259,41 +256,54 @@ class NeuralNetwork(object):
 
         return J
 
-    def checkGradient(self):
+    def checkGradiends(self):
+        # for i in range(len(self.theta)):
+        #     print('\t\tTheta{0}='.format(i+1))
+        #     for j in range(len(self.theta[i])):
+        #         print('\t\t\t{0}'.format(list(self.theta[i][j])))
+
         # Compara os valores obtidos pelo backpropagation com as derivadas reais
-        print('--------------------------------------------')
-        print('Rodando verificação numérica de gradientes (epsilon={0})'.format(self.epsilon))
+        print('\n\n--------------------------------------------')
+        print('Rodando verificação numérica de gradientes (epsilon = {0})'.format(self.epsilon))
 
         numeric_gradients = []
         for i in range(len(self.neurons_per_layer) - 1):
             numeric_gradients.append(np.zeros(shape=(self.neurons_per_layer[i+1], self.neurons_per_layer[i]+1)))
 
-        for i in range(len(self.initial_theta)):
+        thetas = list(self.theta)
+        for i in range(len(self.theta)):
             # Para cada theta
-            for j in range(len(self.initial_theta[i])):
-                for k in range(len(self.initial_theta[i][j])):
-                    original_theta = self.initial_theta[i][j][k]
-                    self.theta[i][j][k] = self.theta[i][j][k] + self.epsilon
-                    j_plus_epsilon = self.backpropagation(should_print=False)
+            for j in range(len(self.theta[i])):
+                for k in range(len(self.theta[i][j])):
+                    original_theta = self.theta[i][j][k]
+                    thetas[i][j][k] = thetas[i][j][k] + self.epsilon
+                    j_plus_epsilon = self.backpropagation(thetas, should_print=False)
 
-                    self.theta[i][j][k] = original_theta - self.epsilon
-                    j_minus_epsilon = self.backpropagation(should_print=False)
+                    thetas[i][j][k] = original_theta - self.epsilon
+                    j_minus_epsilon = self.backpropagation(thetas, should_print=False)
 
                     numeric_gradients[i][j][k] = (j_plus_epsilon - j_minus_epsilon)/(2 * self.epsilon)
 
 
-        for i in range(len(self.theta)):
-            print('Gradientes numéricos de theta {0}'.format(i))
+        for i in range(len(thetas)):
+            print('Gradientes numéricos de theta {0}'.format(i+1))
             print(numeric_gradients[i])
 
 
-        print('--------------------------------------------')
+        print('\n\n--------------------------------------------')
         print('Verificando corretude dos gradientes com base nos gradientes numericos:')
 
+        # Atualiza os valores com os valores conseguindos pelo backpropagation original
+        self.runBackpropagation(should_print=False)
 
-        for i in range(len(self.theta)):
+        for i in range(len(thetas)):
             error = abs(np.sum(numeric_gradients[i] - self.regularized_gradients[i]))
             print('Erro entre gradiente via backprop e gradiente numerico para Theta {0}: {1}'.format(i, error))
+
+
+
+    def runBackpropagation(self, should_print=True):
+        self.backpropagation(thetas=self.theta, should_print=should_print)
 
 
     def runNetwork(self, max_iter):
@@ -302,7 +312,7 @@ class NeuralNetwork(object):
 
         for i in range(max_iter):
             j = new_j
-            new_j = self.backpropagation()
+            new_j = self.backpropagation(thetas=self.theta)
 
             if abs(j - new_j) < self.epsilon:
                 return self.theta